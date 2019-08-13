b3c0512aa5184269807a7e2b7dac7024914439ff
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-13 00:46:51 +0000

    Automatically backed up by Learn

--- lib/game.rb
+++ lib/game.rb
@@ -209,7 +209,8 @@ class Game
           puts "Option 1 selected, Player #{player_selection} will play first"
         when "2"
           puts "Option 2 selected, Player #{player_selection} will play first"
-          newgame = Game.new(player_1, player_2).play
+          newgame = Game.new(player_1, player_2)
+          newgame.play
       end
     end
   end
